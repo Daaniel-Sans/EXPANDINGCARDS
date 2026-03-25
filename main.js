6768b20cf07898c465fa89d38c717b55a8aae4a7
Daaniel-Sans <danpavtel@gmail.com>
2026-03-25 11:39:15 +0100

    feat: implement interactive expanding cards with active state transitions

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 const panels = document.querySelectorAll('.panel');
-
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+        removeActiveClasses();
+        panel.classList.add('active');
+        panel.querySelector('button').textContent = '–';
+    });
+});
 
-    function.removeActiveClasses() {
-        panels.forEach(panel => {
-            panel.classList.remove('active')
-    })
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+        panel.querySelector('button').textContent = '+';
+    });
 }
-
-
